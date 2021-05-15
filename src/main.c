b0342008b035f76ef3cd532b3885d6b14e61f0c0
cpizer <59721050+cpizer@users.noreply.github.com>
2021-05-15 23:28:07 +0200

    made it run

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "flite.h"
 
 cst_voice * register_cmu_us_kal();
+cst_voice * register_cmu_us_awb();
 
 int main(void)
 {
@@ -9,6 +10,10 @@ int main(void)
     flite_init();
 
     v = register_cmu_us_kal(NULL);
+    flite_text_to_speech("This is a test of the flite c-library", v, "play");
+
+    v = register_cmu_us_awb(NULL);
+    //v = flite_voice_select("awb");
 
     flite_text_to_speech("This is a test of the flite c-library", v, "play");
 
